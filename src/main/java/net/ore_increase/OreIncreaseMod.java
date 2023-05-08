cec4259933c59046a2ea0e64a058982e1f0d8418
msNPS <adgrom@mail.ru>
2023-05-08 19:48:00 +0300

    Project Published

---
+++ src/main/java/net/ore_increase/OreIncreaseMod.java
@@ -0,0 +1,20 @@
+package net.ore_increase;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.ore_increase.util.LootTableModifiers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.ore_increase.util.LootTableModifiers;
+
+public class OreIncreaseMod implements ModInitializer {
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("ore-increase");
+
+    @Override
+    public void onInitialize() {
+        LootTableModifiers.modifyLootTables();
+    }
+}
